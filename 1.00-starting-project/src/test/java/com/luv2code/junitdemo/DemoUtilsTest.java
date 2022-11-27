a158a9025cf5d26e62ae81f4016c135ad8392a85
dhlee3994 <dhlee3994@gmail.com>
2022-11-27 17:50:28 +0900

    step06: assertThrows(assertDoesNotThrow) & assertTimeoutPreemptively

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,7 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -81,5 +82,25 @@ class DemoUtilsTest {
         List<String> theList = List.of("luv", "2", "code");
 
         assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should be match");
+    }
+
+    @DisplayName("Throws and Does Not Throw")
+    @Test
+    void testThrowsAndDoseNotThrow() {
+        assertThrows(Exception.class, () -> {
+            demoUtils.throwException(-1);
+        }, "Should throw exception");
+
+        assertDoesNotThrow(() -> {
+            demoUtils.throwException(5);
+        }, "Should not throw exception");
+    }
+
+    @DisplayName("Timeout")
+    @Test
+    void testTimeout() {
+        assertTimeoutPreemptively(Duration.ofSeconds(3), () -> {
+            demoUtils.checkTimeout();
+        }, "Method should execute in 3 seconds");
     }
 }
