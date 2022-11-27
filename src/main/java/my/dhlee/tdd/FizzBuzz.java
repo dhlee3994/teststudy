d99f8bd29dd117bcd5d510749308c674a1eee44a
dhlee3994 <dhlee3994@gmail.com>
2022-11-27 22:43:52 +0900

    refactor: FizzBuzz

--- src/main/java/my/dhlee/tdd/FizzBuzz.java
+++ src/main/java/my/dhlee/tdd/FizzBuzz.java
@@ -7,14 +7,20 @@ package my.dhlee.tdd;
 public class FizzBuzz {
 
     public static String compute(int i) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            return "FizzBuzz";
-        } else if (i % 3 == 0) {
-            return "Fizz";
-        } else if (i % 5 == 0) {
-            return "Buzz";
-        } else {
-            return Integer.toString(i);
+
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
         }
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if (result.length() == 0) {
+            result.append(i);
+        }
+
+        return result.toString();
     }
 }
