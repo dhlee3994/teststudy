f492a07d7e534efb13b1e88f10f6b28d252ab470
dhlee3994 <dhlee3994@gmail.com>
2022-12-03 20:16:49 +0900

    Mock Test 준비

--- src/main/java/my/dhlee/TeststudyApplication.java
+++ src/main/java/my/dhlee/TeststudyApplication.java
@@ -1,6 +1,8 @@
 package my.dhlee;
 
+import my.dhlee.dao.ApplicationDao;
 import my.dhlee.models.CollegeStudent;
+import my.dhlee.service.ApplicationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,18 @@ public class TeststudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeststudyApplication.class, args);
+	}
+
+	/* New for Section 2.2 */
+	@Bean(name = "applicationExample")
+	ApplicationService getApplicationService() {
+		return new ApplicationService();
+	}
+
+	/* New for Section 2.2 */
+	@Bean(name = "applicationDao")
+	ApplicationDao getApplicationDao() {
+		return new ApplicationDao();
 	}
 
 	@Bean(name = "collegeStudent")
---
+++ src/main/java/my/dhlee/service/ApplicationService.java
@@ -0,0 +1,25 @@
+package my.dhlee.service;
+
+import my.dhlee.dao.ApplicationDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ApplicationService {
+
+    @Autowired
+    private ApplicationDao applicationDao;
+
+    public double addGradeResultsForSingleClass(List<Double> numbers) {
+        return applicationDao.addGradeResultsForSingleClass(numbers);
+    }
+
+    public double findGradePointAverage (List<Double> grades ) {
+        return applicationDao.findGradePointAverage(grades);
+    }
+
+    public Object checkNull(Object obj) {
+        return applicationDao.checkNull(obj);
+    }
+
+}
